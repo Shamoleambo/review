5602c2b0914dc52d886ae84031c750ada4ca9875
Tiago Gomes <tidgomes@gmail.com>
2026-02-23 17:12:31 -0300

    when control structure

--- kotlin/kotlin_fcc/src/Main.kt
+++ kotlin/kotlin_fcc/src/Main.kt
@@ -2,6 +2,11 @@ val name: String = "James"
 var greeting: String? = "hello"
 
 fun main() {
+
+    when(greeting){
+        null -> println("Oh snap")
+        "hello" -> println(greeting)
+    }
     println(name)
     println(greeting)
 
